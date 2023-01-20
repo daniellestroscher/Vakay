c0221e3fcfc38f023fd7f97326e7e70de812de20
Danielle Stroscher <daniellejs98@gmail.com>
2023-01-20 13:44:21 -0800

    firebase config update

--- middleware/firebase-config.ts
+++ middleware/firebase-config.ts
@@ -5,13 +5,14 @@ import { getAuth } from 'firebase-admin/auth';
 let app: firebase.App;
 if (firebase.getApps().length === 0) {
   app = firebase.initializeApp({
-    credential: admin.credential.cert({
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY
-        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, '\n')
-        : undefined,
-      projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    }), //applicationDefault()
+    credential: admin.credential //.applicationDefault(),
+      .cert({
+        projectId: 'tripster-85894',
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY
+          ? (process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, '\n') as string)
+          : undefined,
+      }),
     databaseURL: process.env.MONGODB_URI,
   });
 } else {
